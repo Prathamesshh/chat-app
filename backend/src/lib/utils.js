10fc04ff4f5ecca3ac949417e4a101e562ba66ae
Prathamesshh <prathameshkurekar@gmail.com>
2025-03-23 23:29:53 +0530

    file to create a token which would request to frontend

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) => {
-      const token = jwt.sign({userId}, process.env.JWT_SECRET, 
-        {expiresIn: '10d'}); //Cookie will expire in 10 days from the time it is set on the client side 
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '10d',
+  });
 
-        res.cookie("jwt",token,{
-            maxAge: 7 * 24 * 60 * 60* 1000,
-            httpOnly: true, //prevent XSS attacks by not allowing the client side to access the cookie
-            sameSite:"strict", //prevent CSRF attacks by ensuring that the cookie is sent only to the same site that set it
-            secure:process.env.NODE_ENV === "development",
-        });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // <--- FIXED
+    sameSite: "lax", // <--- FIXED
+    maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+  });
 
-        return token;
+  return token;
 };
