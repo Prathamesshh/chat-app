8f77bda4f5b69b21296564d98eafb3c40a33f6ae
Prathamesshh <prathameshkurekar@gmail.com>
2025-04-05 23:33:50 +0530

    File for authentication

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,8 @@ import { axiosInstance } from '../lib/axios.js';
 import { toast } from 'react-hot-toast';
 import { io } from 'socket.io-client'; // <-- Add this!
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
