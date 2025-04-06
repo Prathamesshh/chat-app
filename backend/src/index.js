c0d1af5f6f3b8455f5a13c40e7dfe0f952f2a37f
Prathamesshh <prathameshkurekar@gmail.com>
2025-04-06 11:58:39 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit: "30mb", extended: true }));
+app.use(express.urlencoded({limit: "30mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,15 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '10d',
+    expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // <--- FIXED
-    sameSite: "lax", // <--- FIXED
-    maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
